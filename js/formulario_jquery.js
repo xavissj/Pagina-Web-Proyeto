8615c118737124ea3b5ac4ff88c79c99bf30f9e2
Xavier Bonilla <xavier.bonillaplaza@gmail.com>
2021-01-18 23:14:41 -0300

    validacion login

---
+++ js/formulario_jquery.js
@@ -0,0 +1,14 @@
+$('#btnIngresar').on('click', function () {
+
+    var username, password;
+    username = $("#username").val();
+    password = $("contraseña").val();
+
+    
+
+    if (username == "") {
+        alert("Porfavor ingrese un usuario y contraseña valida");
+        return false;
+
+    }    
+});
